3c9b354764776df47f1e130ebaabfdc5efea0fd0
Mohamed Halim <mohamedhalim1998@gmail.com>
2020-10-31 18:01:23 +0200

    finish app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,66 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage("images/error.png"),
+            ),
+            Text(
+              "Mohamed Halim",
+              style: TextStyle(
+                  fontFamily: "DancingScript",
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "MOBILE DEVELOPER",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+            ),
+            SizedBox(
+              width: 100,
+              child: Divider(
+                color: Colors.teal[100],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  "+201202820502",
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  "mohamedhalim1998@gmail.com",
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
